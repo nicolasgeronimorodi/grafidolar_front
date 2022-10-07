b5dd10e3ac16e30850a6a814a073fa53f643fc09
nicolasgeronimorodi <nicolasgeronimorodi@gmail.com>
2022-10-07 01:27:35 -0300

    main image utilizando modulo lazyloadimage . testeo

--- src/components/MainTextImage.js
+++ src/components/MainTextImage.js
@@ -1,28 +1,13 @@
-
-import { Image } from 'react-bootstrap'
-//import Grafidolar_CityPorteña from '../assets/Grafidolar_CityPorteña.png'
-
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import MainImageBootstrapped from "../assets/assets_components/MainImageBootstrapped";
 export default function MainTextImage(){
 
-return (
+return(
 
-
-/*
- <Image
-        src="https://grafidolar-restapi.herokuapp.com/static/images/Grafidolar_CityPorteña.png"
-        fluid
-        rounded
-        className=""
-        />
-
-*/
-<Image
-src='https://i.picsum.photos/id/1022/6000/3376.jpg?hmac=FBA9Qbec8NfDlxj8xLhV9k3DQEKEc-3zxkQM-hmfcy0'
-rounded
-fluid
-width={555}
-height={326}
+<div>
+<LazyLoadImage
+src={MainImageBootstrapped}
 />
-
+</div>
 )
 }
